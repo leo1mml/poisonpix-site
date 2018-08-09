217c0957715b7b1cc2cd241599fc7f1a7cd12e90
Leonel Menezes <mml_leo@hotmail.com>
2018-08-09 00:46:26 -0300

    added navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class NavBar extends React.Component {
+
+    componentDidMount(){
+        window.onscroll = function() {
+            if(window.pageYOffset > 50){
+                document.getElementsByClassName("nav-container")[0].style.top = "-200px";
+            }else {
+                document.getElementsByClassName("nav-container")[0].style.top = "0px";
+            }
+        }
+
+    }
+
+    toggleMenu = () => {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.classList.add("responsive")
+        } else {
+            x.classList.remove("responsive")
+        }
+    }
+
+    render () {
+        return (
+            <div className="nav-container">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+                <div className="topnav" id="myTopnav">
+                    <Link to="/" className="active"><span className="topnav__logo-text">POISONPIX</span></Link>
+                    <div className="nav-links">
+                        <Link className="" onClick={this.toggleMenu} to="/about">About</Link>
+                        <Link className="" onClick={this.toggleMenu} to="/">Portfolio</Link>
+                        <Link className="" onClick={this.toggleMenu} to="/">Contact</Link>
+                    </div>
+                    <a className="icon" onClick={this.toggleMenu}>
+                        <i className="fa fa-bars"></i>
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBar
--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Navbar from '../Components/NavBar';
 import LandingPage from '../Containers/LandingPage';
+
 
 export default () => (
   <div className="app-container">
     <BrowserRouter>
       <div className="container-component-container">
-        {/* <NavBar/> */}
+        <Navbar/>
         <Switch>
             <Route path="/" component={LandingPage} exact={true}/>
         </Switch>
