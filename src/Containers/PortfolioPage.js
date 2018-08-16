21bfa24f572feabd3c38ab3edf695cbc94fdc5c7
Leonel Menezes <mml_leo@hotmail.com>
2018-08-16 02:46:16 -0300

    added preview of descriptions

--- src/Containers/PortfolioPage.js
+++ src/Containers/PortfolioPage.js
@@ -22,16 +22,16 @@ class PortfolioPage extends React.Component{
                             <div className="portfolio__slider__content__slide">
                                 <div className="portfolio__slider__content__slide__game-content portfolio__slider__content__slide__game-content--right">
                                     <div className="portfolio__game-presentation">
-                                        <span className="portfolio__slide__game-title">Iter Inferis</span>
-                                        <p className="portfolio__slide__game-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat obcaecati quisquam quae maiores. Quo numquam perspiciatis quam error, beatae incidunt consequuntur cum asperiores natus hic possimus voluptas molestiae officia odit.</p>
+                                        <span className="portfolio__slide__game-title">ITER INFERIS</span>
+                                        <p className="portfolio__slide__game-description">In a desolated place, whithout a clue about where you are or even who yourself really are. This is a dark place and your're confused. Try to unveil your own mysteries and the ones of this world.</p>
                                         <button className="portfolio__slide__learn-more-btn">Learn More...</button>
                                     </div>
                                     <img src={require('../Images/slides/slide-01.png')} className="portfolio__game__image" alt=""></img>
                                 </div>
                                 <div className="portfolio__slider__content__slide__game-content portfolio__slider__content__slide__game-content--left">
                                     <div className="portfolio__game-presentation">
-                                        <span className="portfolio__slide__game-title">Iter Inferis</span>
-                                        <p className="portfolio__slide__game-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat obcaecati quisquam quae maiores. Quo numquam perspiciatis quam error, beatae incidunt consequuntur cum asperiores natus hic possimus voluptas molestiae officia odit.</p>
+                                        <span className="portfolio__slide__game-title">AMARUQ</span>
+                                        <p className="portfolio__slide__game-description">It's only you and your father, living in the cold of the mountains, everyday he leaves you alone to hunt and come back with food,but one day he doesn't, what happen to him? you have to find it out, but how will you survive alone?</p>
                                         <button className="portfolio__slide__learn-more-btn">Learn More...</button>
                                     </div>
                                     <img src={require('../Images/slides/slide-02.png')} className="portfolio__game__image" alt=""></img>
