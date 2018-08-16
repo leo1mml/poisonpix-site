4d55f21f9aaeedea29ccb2eff60158e74f44c54e
Leonel Menezes <mml_leo@hotmail.com>
2018-08-16 04:12:34 -0300

    typo in description of amaruq

--- src/Containers/PortfolioPage.js
+++ src/Containers/PortfolioPage.js
@@ -31,7 +31,7 @@ class PortfolioPage extends React.Component{
                                 <div className="portfolio__slider__content__slide__game-content portfolio__slider__content__slide__game-content--left">
                                     <div className="portfolio__game-presentation">
                                         <span className="portfolio__slide__game-title">AMARUQ</span>
-                                        <p className="portfolio__slide__game-description">It's only you and your father, living in the cold of the mountains, everyday he leaves you alone to hunt and come back with food,but one day he doesn't, what happen to him? you have to find it out, but how will you survive alone?</p>
+                                        <p className="portfolio__slide__game-description">It's only you and your father, living in the cold of the mountains, everyday he leaves you alone to hunt and comes back with food,but one day he doesn't, what happen to him? you have to find it out, but how will you survive alone?</p>
                                         <button className="portfolio__slide__learn-more-btn">Learn More...</button>
                                     </div>
                                     <img src={require('../Images/slides/slide-02.png')} className="portfolio__game__image" alt=""></img>
