333efe8d0c039a3267dbc0a9df47a14b511304c0
Leonel Menezes <mml_leo@hotmail.com>
2018-08-16 17:32:53 -0300

    slide images improved

--- src/Containers/PortfolioPage.js
+++ src/Containers/PortfolioPage.js
@@ -26,7 +26,7 @@ class PortfolioPage extends React.Component{
                                         <p className="portfolio__slide__game-description">In a desolated place, whithout a clue about where you are or even who yourself really are. This is a dark place and your're confused. Try to unveil your own mysteries and the ones of this world.</p>
                                         <a className="portfolio__slide__learn-more-btn">Coming Soon...</a>
                                     </div>
-                                    <img src={require('../Images/slides/slide-01.png')} className="portfolio__game__image" alt=""></img>
+                                    <img src={require('../Images/portfolio-images/slide-01.png')} className="portfolio__game__image" alt=""></img>
                                 </div>
                                 <div className="portfolio__slider__content__slide__game-content portfolio__slider__content__slide__game-content--left">
                                     <div className="portfolio__game-presentation">
@@ -34,7 +34,7 @@ class PortfolioPage extends React.Component{
                                         <p className="portfolio__slide__game-description">It's only you and your father, living in the cold of the mountains, everyday he leaves you alone to hunt and comes back with food,but one day he doesn't, what happen to him? you have to find it out, but how will you survive alone?</p>
                                         <a  href='https://poisonpix.itch.io/amaruq' target="_blank" rel="noopener noreferrer" className="portfolio__slide__learn-more-btn">Learn More...</a>
                                     </div>
-                                    <img src={require('../Images/slides/slide-02.png')} className="portfolio__game__image" alt=""></img>
+                                    <img src={require('../Images/portfolio-images/slide-02.png')} className="portfolio__game__image" alt=""></img>
                                 </div>
                             </div>
                         </div>
