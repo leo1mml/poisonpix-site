4ba2e30a036945d62c9653224d3fb297eecd4798
Leonel Menezes <mml_leo@hotmail.com>
2018-08-16 21:55:31 -0300

    translation added to portuguese

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,14 +1,37 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import {IntlProvider} from 'react-intl';
+import {IntlProvider, addLocaleData} from 'react-intl';
 import Navbar from '../Components/NavBar';
 import LandingPage from '../Containers/LandingPage';
 import AboutPage from '../Containers/AboutPage';
 import PortfolioPage from '../Containers/PortfolioPage';
 
+import en from "react-intl/locale-data/en";
+import pt from "react-intl/locale-data/pt";
+
+import localeData from "./../../build/locales/data.json";
+
+addLocaleData([...en, ...pt]);
+
+// Define user's language. Different browsers have the user locale defined
+// on different fields on the `navigator` object, so we make sure to account
+// for these different by checking all of them
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+// Split locales with a region code
+const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// Try full locale, try locale without region code, fallback to 'en'
+const messages =
+  localeData[languageWithoutRegionCode] ||
+  localeData[language] ||
+  localeData.en;
 
 export default () => (
-    <IntlProvider>
+    <IntlProvider locale={language} messages={messages}>
         <BrowserRouter>
             <div className="app-container">
                 <Navbar/>
